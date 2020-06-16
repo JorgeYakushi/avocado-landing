3dd86f98a23c5b891f010272e43fde42e061832b
JorgeYakushi <42720518+JorgeYakushi@users.noreply.github.com>
2020-06-16 02:54:09 -0500

    finished carousel slider
    wip: last section and footer

--- scripts.js
+++ scripts.js
@@ -12,6 +12,7 @@ function toggleMenu() {
     console.log(menuItem)
  
   } 
+
 function toggleOnOff(arr, status){
     for(let i = 0; i < arr.length; i++){
         if(status == "toggleOn"){
@@ -24,47 +25,117 @@ function toggleOnOff(arr, status){
 }
 
 
-function colorCarouselNavButtons() {
-    let url = window.location.href;
-    var URL=window.location.href;
-    var arr=URL.split('/')
+    var itemClassName = "carousel__photo";
+    items = document.getElementsByClassName(itemClassName),
+    totalItems = items.length,
+    slide = 0,
+    moving = true;
 
-    let carouselIndex = arr[3];
-    let list = document.getElementById("carousel-nav-list");
-    let items = list.getElementsByTagName("li");
-    for (let i = 0; i< items.length; ++i){
-        let anchor = items[i].getElementsByTagName("a");
-        
-        if (anchor[0].classList.contains("carousel-nav-button-active")){
-            anchor[0].classList.remove("carousel-nav-button-active");
+   // Set classes
+function setInitialClasses() {
+    // Targets the previous, current, and next items
+    // This assumes there are at least three items.  items[totalItems - 1].classList.add("prev");
+    items[0].classList.add("active");
+    items[1].classList.add("next");
+  }// Set event listeners
+  function setEventListeners() {
+    var next = document.getElementsByClassName('carousel__button--next')[0],
+        prev = document.getElementsByClassName('carousel__button--prev')[0];  next.addEventListener('click', moveNext);
+    prev.addEventListener('click', movePrev);
+  }
+
+ // Next navigation handler
+function moveNext() {  // Check if moving
+  if (!moving) {    // If it's the last slide, reset to 0, else +1
+    if (slide === (totalItems - 1)) {
+      slide = 0;
+    } else {
+      slide++;
+    }    // Move carousel to updated slide
+    moveCarouselTo(slide);
+  }
+}// Previous navigation handler
+function movePrev() {  // Check if moving
+  if (!moving) {    // If it's the first slide, set as the last slide, else -1
+    if (slide === 0) {
+      slide = (totalItems - 1);
+    } else {
+      slide--;
+    }
+          
+    // Move carousel to updated slide
+    moveCarouselTo(slide);
+  }
+}
+
+function disableInteraction() {  // Set 'moving' to true for the same duration as our transition.
+// (0.5s = 500ms)
+
+moving = true;  // setTimeout runs its function once after the given time
+setTimeout(function(){
+  moving = false
+}, 500);
+}
+
+function moveCarouselTo(slide) {  // Check if carousel is moving, if not, allow interaction
+    console.log(slide);
+    if(!moving) {    // temporarily disable interactivity
+      disableInteraction();    // Update the "old" adjacent slides with "new" ones
+      var newPrevious = slide - 1,
+          newNext = slide + 1,
+          oldPrevious = slide - 2,
+          oldNext = slide + 2;    // Test if carousel has more than three items
+      if ((totalItems - 1) > 3) {      // Checks and updates if the new slides are out of bounds
+        if (newPrevious <= 0) {
+          oldPrevious = (totalItems - 1);
+        } else if (newNext >= (totalItems - 1)){
+          oldNext = 0;
+        }      // Checks and updates if slide is at the beginning/end
+        if (slide === 0) {
+          newPrevious = (totalItems - 1);
+          oldPrevious = (totalItems - 2);
+          oldNext = (slide + 1);
+        } else if (slide === (totalItems -1)) {
+          newPrevious = (slide - 1);
+          newNext = 0;
+          oldNext = 1;
+        }      // Now we've worked out where we are and where we're going, 
+        // by adding/removing classes we'll trigger the transitions.      // Reset old next/prev elements to default classes
+        items[oldPrevious].className = itemClassName;
+        items[oldNext].className = itemClassName;      // Add new classes
+        items[newPrevious].className = itemClassName + " prev";
+        items[slide].className = itemClassName + " active";
+        items[newNext].className = itemClassName + " next";
+        colorCarouselButton(slide);
+      }
+    }
+  }
+
+ function initCarousel() {
+    setInitialClasses();
+    setEventListeners();  // Set moving to false so that the carousel becomes interactive
+    moving = false;
+  }
+
+  // make it rain
+initCarousel();
+
+document.addEventListener("DOMContentLoaded", function(e) {
+})
+
+function colorCarouselButton(number){
+    let list = document.getElementsByClassName("carousel-nav-button");
+    for(let i = 0; i < list.length; i++){
+        console.log(i);
+        if(list[i].classList.contains("carousel-nav-button-active")){
+            list[i].classList.remove("carousel-nav-button-active");
         }
     }
-    let newAnchor;
-    switch (carouselIndex){
-        case "":
-        case "#carousel-slide-1":
-            newAnchor = items[0].getElementsByTagName("a");
-            newAnchor.classList.contains("carousel-nav-button-active");
-        break;
-        case "#carousel-slide-2":
-            newAnchor = items[1].getElementsByTagName("a");
-            newAnchor.classList.contains("carousel-nav-button-active");
-        break;
-        case "#carousel-slide-3":
-            newAnchor = items[2].getElementsByTagName("a");
-            newAnchor.classList.contains("carousel-nav-button-active");
-        break;
-        case "#carousel-slide-4":
-            newAnchor = items[3].getElementsByTagName("a");
-            newAnchor.classList.contains("carousel-nav-button-active");
-        break;
-        case "#carousel-slide-5":
-            newAnchor = items[4].getElementsByTagName("a");
-            newAnchor.classList.contains("carousel-nav-button-active");
-        break;
+    list[number].classList.add("carousel-nav-button-active");
+}
 
+function moveCarouselFromNavButton(start, end){
+    if(start == end){
+        moveCarouselTo(start);
     }
-    
-
-console.log(newAnchor);
 }
