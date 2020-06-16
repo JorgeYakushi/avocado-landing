00910dc4793ea3191e4bcd785f49ccd72432e077
JorgeYakushi <42720518+JorgeYakushi@users.noreply.github.com>
2020-06-15 19:08:30 -0500

    added slider
    added slider
    still thing to figure out how to add color to nav buttons on slider

--- scripts.js
+++ scripts.js
@@ -23,3 +23,43 @@ function toggleOnOff(arr, status){
     }
 }
 
+
+function colorCarouselNavButtons() {
+    let url = window.location.href;
+    var URL=window.location.href;
+    var arr=URL.split('/')
+
+    let carouselIndex = arr[3];
+    let list = document.getElementById("carousel-nav-list");
+    let items = list.getElementsByTagName("li");
+    for (let i = 0; i< items.length; ++i){
+        let anchor = items[i].getElementsByTagName("a");
+        
+        if (anchor[0].classList.contains("carousel-nav-button-active")){
+            anchor[0].classList.remove("carousel-nav-button-active");
+        }
+    }
+    let newAnchor;
+    switch (carouselIndex){
+        case "":
+        case "#carousel-slide-1":
+            newAnchor = items[0].getElementsByTagName("a");
+        break;
+        case "#carousel-slide-2":
+            newAnchor = items[1].getElementsByTagName("a");
+        break;
+        case "#carousel-slide-3":
+            newAnchor = items[2].getElementsByTagName("a");
+        break;
+        case "#carousel-slide-4":
+            newAnchor = items[3].getElementsByTagName("a");
+        break;
+        case "#carousel-slide-5":
+            newAnchor = items[4].getElementsByTagName("a");
+        break;
+
+    }
+    
+
+console.log(newAnchor);
+}
